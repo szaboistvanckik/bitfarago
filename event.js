3ec44c8ac5d336da67898585a8a8415eed6cc00c
szaboistvanckik <szabo.istvan@ckik.hu>
2025-09-23 18:53:40 +0200

    design

--- event.js
+++ event.js
@@ -5,7 +5,7 @@ function startTime() {
     let s = today.getSeconds();
     m = checkTime(m);
     s = checkTime(s);
-    document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
+    document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
     setTimeout(startTime, 1000);
 }
 
@@ -14,6 +14,20 @@ function checkTime(i) {
     return i;
 }
 
-function kepek() {
-    
+function show(button) {
+    const card = button.parentElement;
+    const ps = card.querySelectorAll('p');
+    const intro = ps[1];
+
+    const cards = document.querySelectorAll('.card');
+    const index = Array.from(cards).indexOf(card);
+
+    const intros = [
+      "Tiszta rosszul vagyok",
+      "Tiszta gyagyás itt minden",
+      "Be van rosszulva tisztára"
+    ];
+
+    intro.innerText = intros[index] || "Bemutatkozás nem elérhető.";
+    intro.classList.add('show');
 }
