6ce767684b5d64a2d5c6186f70e139a18d8ac7c4
szaboistvanckik <szabo.istvan@ckik.hu>
2025-09-29 09:56:19 +0200

    timer

--- event2.js
+++ event2.js
@@ -1,3 +1,19 @@
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};
+    return i;
+}
+
 const taskList = document.getElementById('task-list');
 const newTaskInput = document.getElementById('new-task');
 const addTaskButton = document.getElementById('add-task');
